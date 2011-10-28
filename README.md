ae51743fe82510b5e9750d9e8eda4782c9d97ef0
Darrin Eden <darrin.eden@gmail.com>
2011-10-28 08:53:17 -0700

    locale cookbook

---
+++ README.md
@@ -0,0 +1,19 @@
+Description
+===========
+
+Sets system locale
+
+Requirements
+============
+
+Tested on Ubuntu
+
+Attributes
+==========
+
+* `node[:locale][:lang]` -- defaults to "en_US.UTF-8"
+
+Usage
+=====
+
+Include the default recipe in your run list.
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "Heavy Water Software Inc."
+maintainer_email "darrin@heavywater.ca"
+license          "Apache 2.0"
+description      "Installs/Configures locale"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+supports "ubuntu"
+supports "debian"
