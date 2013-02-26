71d3d8be80c6ccdb8f8016f568dcb1f0c06615b4
Darrin Eden <darrin.eden@gmail.com>
2013-02-25 20:15:46 -0500

    bump version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "darrin@heavywater.ca"
 license          "Apache 2.0"
 description      "Installs/Configures locale"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.1.0"
 
 supports "ubuntu"
 supports "debian"
