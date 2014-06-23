d15dc81f54380c3616004a23307fd8680eedd8e0
Phil Cohen <github@phlippers.net>
2014-06-23 15:33:12 -0700

    don’t raise exception when `file_path` is missing

--- libraries/locale.rb
+++ libraries/locale.rb
@@ -1,6 +1,8 @@
 module Locale
   class << self
     def up_to_date?(file_path, lang, lc_all)
+      return false unless ::File.exist?(file_path)
+
       locale = IO.read(file_path)
       locale.include?("LANG=#{lang}") && locale.include?("LC_ALL=#{lc_all}")
     end
