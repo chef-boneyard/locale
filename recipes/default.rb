f6e5c6c000c18feb28e07b6f8e44b4039b6a6993
Stephen Crawley <s.crawley@uq.edu.au>
2014-07-14 13:35:03 +1000

    Add Fedora test coverage + 2 bug fixes

--- recipes/default.rb
+++ recipes/default.rb
@@ -53,7 +53,7 @@ when 'fedora'
 
   # It is not permitted to set LC_ALL in the locale conf file on Fedora.
   bash 'Update locale' do
-    code "localectl set-locale LANG=${lang}"
+    code "echo localectl set-locale LANG=#{lang} > /tmp/foo ; localectl set-locale LANG=#{lang} >> /tmp/foo 2>&1"
     not_if { Locale.up_to_date?('/etc/locale.conf', lang, nil) }
   end
 end
