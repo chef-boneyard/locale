830aafce93bb44bc834e653ffef2836b29708108
John Dyer <jdyer@tropo.com>
2014-07-23 12:16:53 -0400

    A missing file should be considered false when checking up_to_date?

--- libraries/locale.rb
+++ libraries/locale.rb
@@ -1,8 +1,12 @@
 module Locale
   class << self
     def up_to_date?(file_path, lang, lc_all)
-      locale = IO.read(file_path)
-      locale.include?("LANG=#{lang}") && locale.include?("LC_ALL=#{lc_all}")
+      begin
+        locale = IO.read(file_path)
+        locale.include?("LANG=#{lang}") && locale.include?("LC_ALL=#{lc_all}")
+      rescue Errno::ENOENT
+        false
+      end
     end
   end
 end
