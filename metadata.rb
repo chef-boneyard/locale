e47bc526b82e3d1984782e0e7b88f0d0bc431b00
Lamont Granquist <lamont@scriptkiddie.org>
2016-05-27 15:47:28 -0700

    bumping version
    and nijna fixing the issues and source urls

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license          'Apache 2.0'
 description      'Installs/Configures locale'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.3'
+version          '1.1.0'
 
 supports 'ubuntu'
 supports 'debian'
@@ -13,5 +13,5 @@ supports 'redhat'
 supports 'scientific'
 supports 'fedora'
 
-source_url 'https://github.com/chef-cookbooks/activemq' if respond_to?(:source_url)
-issues_url 'https://github.com/chef-cookbooks/activemq/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/chef-cookbooks/locale' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/locale/issues' if respond_to?(:issues_url)
