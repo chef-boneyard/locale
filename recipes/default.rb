71f2a81c7ab779a9955f85ac97e56c534a326a08
Tim Smith <tsmith@chef.io>
2017-01-17 14:28:09 -0800

    Cookstyle fixes
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,9 @@
 #
-# Cookbook Name:: locale
+# Cookbook:: locale
 # Recipe:: default
 #
-# Copyright 2011-2016, Heavy Water Software Inc.
-# Copyright 2016, Chef Software Inc.
+# Copyright:: 2011-2016, Heavy Water Software Inc.
+# Copyright:: 2016, Chef Software Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
