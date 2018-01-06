7cc32b7c605d4d36c7a9a9ad98e50205fb66ca4e
Dan Webb <dan.webb@damacus.io>
2018-01-06 11:37:01 +0000

    inspec --> bats?
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- test/fixtures/cookbooks/test/recipes/default.rb
+++ test/fixtures/cookbooks/test/recipes/default.rb
@@ -1,19 +1,1 @@
 locale 'default'
-
-file_content = <<-EOH
-function setup {
-  [ -f /etc/default/locale ] && . /etc/default/locale
-  [ -f /etc/locale.conf ] && . /etc/locale.conf
-  [ -f /etc/sysconfig/i18n ] && . /etc/sysconfig/i18n
-  true;
-}
-
-setup
-EOH
-
-file '/tmp/setup_locale.sh' do
-  content file_content
-  mode '0755'
-  owner 'root'
-  group 'root'
-end
