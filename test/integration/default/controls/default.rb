9bc95edfbf3702ac321996bb2770a4b93442c6b2
Dan Webb <dan.webb@damacus.io>
2018-01-06 10:51:49 +0000

    Fix return statements in control
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- test/integration/default/controls/default.rb
+++ test/integration/default/controls/default.rb
@@ -1,28 +1,28 @@
 # frozen_string_literal: true
-if ::File.exist?('/etc/default/locale')
-  locale_file = '/etc/default/locale'
-elsif ::File.exist?('/etc/locale.conf')
-  locale_file = '/etc/locale.conf'
-else
-  locale_file = '/etc/sysconfig/i18n'
-end
+locale_file = if ::File.exist?('/etc/default/locale')
+                '/etc/default/locale'
+              elsif ::File.exist?('/etc/locale.conf')
+                '/etc/locale.conf'
+              else
+                '/etc/sysconfig/i18n'
+              end
 
 control 'Set LANG using default resource values' do
   title 'LANG should be en_US.utf8'
 
-  desc "Locale setting LANG"
+  desc 'Locale setting LANG'
   describe bash(". #{locale_file} && locale") do
     its('exit_status') { should eq 0 }
-    its('stdout') { should match /LANG=en_US.utf8/  }
+    its('stdout') { should match /LANG=en_US.utf8/ }
   end
 end
 
 control 'Set LC_ALL using default resource values' do
   title 'LC_ALL should be en_US.utf8'
 
-  desc "Locale setting LC_ALL"
+  desc 'Locale setting LC_ALL'
   describe bash('. #{locale_file} && locale') do
     its('exit_status') { should eq 0 }
-    its('stdout') { should match /LC_ALL=en_US.utf8/  }
+    its('stdout') { should match /LC_ALL=en_US.utf8/ }
   end
 end
