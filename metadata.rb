008f26c537a175c8b2432dfdb6ba1285116621cd
Tim Smith <tsmith@chef.io>
2018-01-05 21:31:48 -0800

    Update the platforms we support
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -6,13 +6,9 @@ description      'Configures the system locale on Linux systems'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.1'
 
-supports 'ubuntu'
-supports 'debian'
-supports 'centos'
-supports 'redhat'
-supports 'scientific'
-supports 'fedora'
-supports 'oracle'
+%w(amazon centos debian fedora opensuse opensuseleap oracle redhat scientific suse ubuntu zlinux).each do |os|
+  supports os
+end
 
 source_url 'https://github.com/chef-cookbooks/locale'
 issues_url 'https://github.com/chef-cookbooks/locale/issues'
