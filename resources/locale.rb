7ebc9edb91797acbc14dd3422b78958d20d0b039
Tim Smith <tsmith@chef.io>
2018-01-06 20:27:05 -0800

    Avoid trying to set locale every time on systemd hosts
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- resources/locale.rb
+++ resources/locale.rb
@@ -64,7 +64,7 @@ action_class do
   def up_to_date?(file_path, lang, lc_all = nil)
     locale = IO.read(file_path)
     locale.include?("LANG=#{lang}") &&
-      (lc_all.nil? || locale.include?("LC_ALL=#{lc_all}"))
+      (node['init_package'] == 'systemd' || lc_all.nil? || locale.include?("LC_ALL=#{lc_all}"))
   rescue
     false
   end
